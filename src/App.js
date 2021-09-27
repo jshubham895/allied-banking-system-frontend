b83270401f8a1fd1ed14f4f79770201103051380
jshubham895 <jshubham895@gmail.com>
2021-09-27 16:48:42 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-
-                                    // Task 1 : Basic Banking System
-                                    // Name-Shubham Jain
-
+// Task 1 : Basic Banking System
+// Name-Shubham Jain
 
 import React from "react";
 import "./App.css";
@@ -19,53 +17,53 @@ import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import {
-  faTrashAlt,
-  faEdit,
-  faMoneyBill,
-  faPlus,
-  faRupeeSign,
-  faUniversity,
-  faUserLock,
-  faUserShield,
-  faTachometerAlt,
+	faTrashAlt,
+	faEdit,
+	faMoneyBill,
+	faPlus,
+	faRupeeSign,
+	faUniversity,
+	faUserLock,
+	faUserShield,
+	faTachometerAlt
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
-  fab,
-  faTachometerAlt,
-  faTrashAlt,
-  faUserShield,
-  faEdit,
-  faMoneyBill,
-  faPlus,
-  faRupeeSign,
-  faUniversity,
-  faUserLock
+	fab,
+	faTachometerAlt,
+	faTrashAlt,
+	faUserShield,
+	faEdit,
+	faMoneyBill,
+	faPlus,
+	faRupeeSign,
+	faUniversity,
+	faUserLock
 );
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="mt-auto">
-          <Switch>
-            <Route exact path="/accounts" component={Accounts} />
-            <Route exact path="/transactions" component={Transcations} />
-            <Route exact path="/accounts/add" component={AddAccount} />
-            <Route
-              exact
-              path="/accounts/edit/:accountId"
-              component={EditAccount}
-            />
-            <Route exact path="/accounts/view/:accountId" component={Account} />
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+				<div className="mt-5">
+					<Switch>
+						<Route exact path="/accounts" component={Accounts} />
+						<Route exact path="/transactions" component={Transcations} />
+						<Route exact path="/accounts/add" component={AddAccount} />
+						<Route
+							exact
+							path="/accounts/edit/:accountId"
+							component={EditAccount}
+						/>
+						<Route exact path="/accounts/view/:accountId" component={Account} />
+						<Route exact path="/" component={Home} />
+					</Switch>
+				</div>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
