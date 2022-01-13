9e5618ce5c0f82738e2fd2ea5ed9b31987fd4f08
jshubham895 <jshubham895@gmail.com>
2022-01-14 00:54:25 +0530

    added loading screen

--- src/components/pages/Accounts.js
+++ src/components/pages/Accounts.js
@@ -7,9 +7,14 @@ import formatter from "../../formatter";
 
 function Accounts() {
 	const [accountsList, setAccountsList] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		getAccounts();
+		const fetchData = async () => {
+			await getAccounts();
+			setLoading(false);
+		};
+		fetchData();
 	}, []);
 
 	const getAccounts = async () => {
@@ -27,8 +32,40 @@ function Accounts() {
 		getAccounts();
 	};
 
+	const LoadingScreen = () => {
+		return (
+			<div className="divLoader">
+				<svg
+					className="svgLoader"
+					viewBox="0 0 100 100"
+					width="10em"
+					height="10em"
+				>
+					<path
+						stroke="none"
+						d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50"
+						fill="#51CACC"
+						transform="rotate(179.719 50 51)"
+					>
+						<animateTransform
+							attributeName="transform"
+							type="rotate"
+							calcMode="linear"
+							values="0 50 51;360 50 51"
+							keyTimes="0;1"
+							dur="1s"
+							begin="0s"
+							repeatCount="indefinite"
+						></animateTransform>
+					</path>
+				</svg>
+			</div>
+		);
+	};
+
 	return (
 		<div className="container">
+			{loading ? <LoadingScreen /> : null}
 			<hr />
 			<Link className="btn-primary btn" to="/">
 				Back
